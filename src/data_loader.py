e878516466f8099b2e05957158d120cb873f114a
Shramanth P. Acharya <140807176+Spacharya005@users.noreply.github.com>
2026-04-11 11:52:08 +0530

    fix: removed hardcoded local path completely

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,23 +1,6 @@
-# import pandas as pd
-
-# def load_data(path):
-
-#     df = pd.read_csv(path)
-#     print(df.columns)
-#     # Combine date + time
-#     df['datetime'] = pd.to_datetime(
-#         df['transaction_date'] + ' ' + df['transaction_time']
-#     )
-
-#     # Revenue
-#     df['revenue'] = df['transaction_qty'] * df['unit_price']
-
-#     return df
-
 import pandas as pd
 
 def load_data(path):
-    path=r"C:\Users\Shramanth P Acharya\OneDrive\Documents\Project\Unified Mentor Private\coffee_demand_forecasting - Copy\data\cofee_sales.csv"
     # ✅ Use dynamic path
     df = pd.read_csv(path)
 
